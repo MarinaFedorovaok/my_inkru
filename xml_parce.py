7841855449126913d22d00176081215b9b3a7305
MarinaFedorovaok <fedma001@mail.ru>
2022-03-21 20:45:40 +0300

    text founded

--- xml_parce.py
+++ xml_parce.py
@@ -4,9 +4,6 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('beerJournal.xml')
 root = tree.getroot()
 print(type(root))
-# p = tree.find('verb')
-# for i in p:
-#     print (p[i].text)
 for child in root:
     print(child.tag, child.attrib)
-#print(root[0][1].text)
+    print(root[0][0].text)
