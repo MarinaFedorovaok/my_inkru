e553084d11f0a7276f69db0eee3dc08ece62676c
MarinaFedorovaok <fedma001@mail.ru>
2022-03-21 21:27:33 +0300

    changes the result file right

--- change.py
+++ change.py
@@ -3,14 +3,13 @@ file_out = open("text_result.txt", "w")
 while True:
     # считываем строку
     line = f.readline()
-    text = line.replace("is", "was")
+    text = line.replace(" is ", " was ")
     file_out.write(text)
 
     # прерываем цикл, если строка пустая
     if not line:
         break
-    # выводим строку
-    print(line.strip())
+   
 
 # закрываем файл
 f.close
