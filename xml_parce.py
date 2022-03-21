f843894b8efbf7dc853524dc36444bc067ee5c14
MarinaFedorovaok <fedma001@mail.ru>
2022-03-21 20:33:17 +0300

    works with root

--- xml_parce.py
+++ xml_parce.py
@@ -1,6 +1,12 @@
-from xml.etree.ElementTree import ElementTree
-tree = ElementTree()
-tree.parse("beerJournal.xml")
-p = tree.find('verb')
-for i in p:
-    print (p[i].text)
+import xml.etree.ElementTree as ET
+#tree = ElementTree()
+#tree.parse("beerJournal.xml")
+tree = ET.parse('beerJournal.xml')
+root = tree.getroot()
+print(type(root))
+# p = tree.find('verb')
+# for i in p:
+#     print (p[i].text)
+for child in root:
+    print(child.tag, child.attrib)
+#print(root[0][1].text)
