c73318ae2cf1ad388f83941286503bb7ab28b0f9
MarinaFedorovaok <fedma001@mail.ru>
2022-03-21 21:41:53 +0300

    method changes works

--- change.py
+++ change.py
@@ -1,16 +1,25 @@
 f = open('text.txt', 'r')
 file_out = open("text_result.txt", "w")
-while True:
-    # считываем строку
-    line = f.readline()
-    text = line.replace(" is ", " was ")
-    file_out.write(text)
+# change_what = open('change_what.txt', 'r')
+# change_to = open('change_to.txt', 'r')
+def change_word (was, will):
+    while True:
+        # считываем строку
+        line = f.readline()
+        # what = change_what.readline()
+        # print(what)
+        # to = change_to.readline()
+        # print(to)
+        text = line.replace(was, will)
+        file_out.write(text)
 
-    # прерываем цикл, если строка пустая
-    if not line:
-        break
+        # прерываем цикл, если строка пустая
+        if not line:
+            break
    
-
-# закрываем файл
+# закрываем файлы
 f.close
 file_out.close
+change_word(' is ', ' was ')
+# change_to.close
+# change_what.close
