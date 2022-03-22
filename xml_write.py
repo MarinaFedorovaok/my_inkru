67c684ef2f14f2b8e575c8ffa8604d3ebb5f6cbc
MarinaFedorovaok <fedma001@mail.ru>
2022-03-22 17:50:03 +0300

    changes maked but not in right place

--- xml_write.py
+++ xml_write.py
@@ -1,10 +1,19 @@
+from logging import root
 import xml.etree.ElementTree as ET
-def write_xml(file_to_parce, file_to_join):
-    tree = ET.parse(file_to_parce)
-    root = tree.getroot()
-    #print(type(root))
-    f = open(file_to_join, 'r')
-    for child in root:
-        #print(child.tag, child.attrib)
-        root[0][0].text = f.readline
-write_xml('beerJournal.xml', 'text_result.txt')
+f = open('text_result.txt', 'r')
+tree = ET.parse('beerJournal.xml')
+root = tree.getroot()
+for beer in root.iter('Beer'):
+    beer.text = f.readline()
+tree.write('output.xml')
+
+# def write_xml(file_to_parce, file_to_join):
+#     tree = ET.parse(file_to_parce)
+#     root = tree.getroot()
+#     #print(type(root))
+#     f = open(file_to_join, 'r')
+#     for child in root:
+#         #print(child.tag, child.attrib)
+#         root[0][0].text = f.readline
+#     tree.write('result.xml')
+# write_xml('beerJournal.xml', 'text_result.txt')
