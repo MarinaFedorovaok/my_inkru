add6bbd4e1e049ea16ef1938d6e6159cc45979de
MarinaFedorovaok <fedma001@mail.ru>
2022-03-24 19:16:16 +0300

    with recurtion

--- xml_parce.py
+++ xml_parce.py
@@ -1,4 +1,11 @@
 import xml.etree.ElementTree as ET
+def parce_subtree(root):
+    # обрабатываем текующую вершину
+    print(root.tag, ' : ', root.text)
+    for child in root:
+        # обрабатываем ее сыновей 
+        parce_subtree(child)
+
 def parce_xml(file):
     tree = ET.parse(file)
     root = tree.getroot()
@@ -10,4 +17,7 @@ def parce_xml(file):
         #print(child.tag, child.attrib)
         f.write(root[i][0].text)
         i +=1
-parce_xml('beerJournal.xml')
+#parce_xml('beerJournal.xml')
+tree = ET.parse('beerJournal.xml')
+root = tree.getroot()
+parce_subtree(root)
