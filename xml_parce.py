7b59af0dda4e135cf8d8a1c8501ff7a48ae685d6
MarinaFedorovaok <fedma001@mail.ru>
2022-03-24 23:43:04 +0300

    changes output

--- xml_parce.py
+++ xml_parce.py
@@ -1,16 +1,28 @@
 import xml.etree.ElementTree as ET
+
+def replacement (dictionary, str):
+    if str in dictionary:
+        return (dictionary[str])
+    else:
+        return str
+
 def parce_subtree(node):
     # обрабатываем текующую вершину
     if 'tspan' in node.tag and node.text != None:
-        node.text = 'Hello, world'
         print(node.text)
+        node.text = replacement(dictionary, node.text)
     for child in node:
         # обрабатываем ее сыновей 
         parce_subtree(child)
 
+dictionary = {
+    "templateText1" : 'Hello',
+    "templateText2" : 'world'}
+
 tree = ET.parse('template.svg')
 root = tree.getroot()
 parce_subtree(root)
+
 tree.write('output_write.svg')
 
 #Отличная справка: https://olegmax.readthedocs.io/ru/latest/represents-2.html
