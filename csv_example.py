b082fd942a1f5ac21264b1bf34caa36cf4eb2a68
MarinaFedorovaok <fedma001@mail.ru>
2022-04-06 22:54:16 +0300

    changes works

--- csv_example.py
+++ csv_example.py
@@ -1,15 +1,17 @@
 import csv
 from operator import length_hint
 import string
-dictionary = {}
-list_of_dictionaries = []
-with open('user_file.csv', newline='') as csvfile:
-    spamreader = list(csv.reader(csvfile, delimiter=",", quotechar='"'))
-    for j in range (len(spamreader)):
-        for i in range (len(spamreader[0])):
-            dictionary[spamreader[0][i]] = spamreader[j][i]
-        list_of_dictionaries.append(dictionary)
-    #print (list(spamreader))
-    for row in spamreader:
-        print(', '.join(row))
-print(list_of_dictionaries)
+
+def csv_to_dictionaries (file_csv):
+    dictionary = {}
+    list_of_dictionaries = []
+    with open(file_csv, newline='') as csvfile:
+        spamreader = list(csv.reader(csvfile, delimiter=",", quotechar='"'))
+        for j in range (1, len(spamreader)):
+            for i in range (len(spamreader[0])):
+                dictionary[spamreader[0][i]] = spamreader[j][i]
+            list_of_dictionaries.append(dictionary.copy())
+        #print (list(spamreader))
+        for row in spamreader:
+            print(', '.join(row))
+    return list_of_dictionaries
--- xml_parce.py
+++ xml_parce.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+import csv_example as csv
 
 def replacement (dictionary, str):
     if str in dictionary:
@@ -9,21 +10,26 @@ def replacement (dictionary, str):
 def parce_subtree(node):
     # обрабатываем текующую вершину
     if 'tspan' in node.tag and node.text != None:
-        print(node.text)
+        #print(node.text)
         node.text = replacement(dictionary, node.text)
     for child in node:
         # обрабатываем ее сыновей 
         parce_subtree(child)
 
-dictionary = {
-    "templateText1" : 'Hello',
-    "templateText2" : 'world'}
+# dictionary = {
+#     "templateText1" : 'Hello',
+#     "templateText2" : 'world'}
 
-tree = ET.parse('template.svg')
-root = tree.getroot()
-parce_subtree(root)
-
-tree.write('output_write.svg')
+file_csv = 'user_file.csv'
+list_dict = csv.csv_to_dictionaries (file_csv)
+#print(list_dict)
+for i in range (len(list_dict)):
+    dictionary = list_dict[i]
+    print(dictionary)
+    tree = ET.parse('template.svg')
+    root = tree.getroot()
+    parce_subtree(root)
+    tree.write('output_write_' + str(i) + '.svg')
 
 #Отличная справка: https://olegmax.readthedocs.io/ru/latest/represents-2.html
 
