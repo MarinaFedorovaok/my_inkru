4c8abb100f7c3a3852664f9361d688ddf134b2e2
MarinaFedorovaok <fedma001@mail.ru>
2022-04-06 21:29:48 +0300

    csv to dictionaries

--- csv_example.py
+++ csv_example.py
@@ -1,5 +1,15 @@
 import csv
-with open('eggs.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=",", quotechar='"')
+from operator import length_hint
+import string
+dictionary = {}
+list_of_dictionaries = []
+with open('user_file.csv', newline='') as csvfile:
+    spamreader = list(csv.reader(csvfile, delimiter=",", quotechar='"'))
+    for j in range (len(spamreader)):
+        for i in range (len(spamreader[0])):
+            dictionary[spamreader[0][i]] = spamreader[j][i]
+        list_of_dictionaries.append(dictionary)
+    #print (list(spamreader))
     for row in spamreader:
         print(', '.join(row))
+print(list_of_dictionaries)
