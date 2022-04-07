6a2d5e980aaaf3113355aabf8f4f151caf1dffcd
MarinaFedorovaok <fedma001@mail.ru>
2022-04-07 18:11:36 +0300

    changing works

--- args.py
+++ args.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from argparse import ArgumentParser, FileType
+from re import template
 import xml_parce as xml
 import csv_example as csv
 import xml.etree.ElementTree as ET
@@ -9,23 +10,22 @@ def line_to_dictionaty():
     parser = ArgumentParser()
     parser.add_argument("--file", dest="filename",
                         help="write report to FILE", metavar="FILE")
-    parser.add_argument('inputfile', nargs=1, type=FileType('r'), help = "template file", metavar="template_file")
+    parser.add_argument('input_file', nargs=1, help = "template file", metavar="template_file")
 
     return vars(parser.parse_args())
 
-#print(line_to_dictionaty())
-
 import xml_parce as xml
 args_dict = line_to_dictionaty()
+print(args_dict)
 file_csv = args_dict['filename']
 #print (file_csv)
-list_dict = csv.csv_to_dictionaries (file_csv)
+list_dict = csv.csv_to_dictionaries(file_csv)
 #print(list_dict)
 
 for i in range (len(list_dict)):
     dictionary = list_dict[i]
     print(dictionary)
-    tree = ET.parse('template.svg')
+    tree = ET.parse(args_dict['input_file'][0])
     root = tree.getroot()
     xml.parce_subtree(root, dictionary)
     tree.write('output_write_' + str(i) + '.svg')
