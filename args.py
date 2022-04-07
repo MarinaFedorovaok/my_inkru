057d0177bc1afdeee4562bbf66cf4e18a7fee8a4
MarinaFedorovaok <fedma001@mail.ru>
2022-04-07 17:27:12 +0300

    commamd line arg works

--- args.py
+++ args.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 from argparse import ArgumentParser
 import xml_parce as xml
+import csv_example as csv
+import xml.etree.ElementTree as ET
+import csv_example as csv
 
 def line_to_dictionaty():
     parser = ArgumentParser()
@@ -14,3 +17,17 @@ def line_to_dictionaty():
 
 print(line_to_dictionaty())
 
+import xml_parce as xml
+args_dict = line_to_dictionaty()
+file_csv = args_dict['filename']
+#print (file_csv)
+list_dict = csv.csv_to_dictionaries (file_csv)
+#print(list_dict)
+
+for i in range (len(list_dict)):
+    dictionary = list_dict[i]
+    print(dictionary)
+    tree = ET.parse('template.svg')
+    root = tree.getroot()
+    xml.parce_subtree(root, dictionary)
+    tree.write('output_write_' + str(i) + '.svg')
--- xml_parce.py
+++ xml_parce.py
@@ -7,25 +7,17 @@ def replacement (dictionary, str):
     else:
         return str
 
-def parce_subtree(node):
+def parce_subtree(node, dictionary):
     # обрабатываем текующую вершину
     if 'tspan' in node.tag and node.text != None:
         #print(node.text)
         node.text = replacement(dictionary, node.text)
     for child in node:
         # обрабатываем ее сыновей 
-        parce_subtree(child)
+        parce_subtree(child, dictionary)
 
-file_csv = 'user_file.csv'
-list_dict = csv.csv_to_dictionaries (file_csv)
+
 #print(list_dict)
-for i in range (len(list_dict)):
-    dictionary = list_dict[i]
-    print(dictionary)
-    tree = ET.parse('template.svg')
-    root = tree.getroot()
-    parce_subtree(root)
-    tree.write('output_write_' + str(i) + '.svg')
 
 #Отличная справка: https://olegmax.readthedocs.io/ru/latest/represents-2.html
 
